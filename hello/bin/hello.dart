43f6c1e2eea75d9f3e891cb6b6af373bb42eb931
Abakarov <zhabaeda@gmail.com>
2025-04-18 16:33:31 +0300

    functions_dart

--- hello/bin/hello.dart
+++ hello/bin/hello.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ffi';
+
 void main() {
  final todayDate = DateTime.now();
  print(todayDate);
@@ -38,7 +40,7 @@ switch(num)
   default:
   print("чиcло не равно 1,2,3");
 }
-for(int i = 1; i<10; i++){
+for(int i = 1; i<5; i++){
   print("$i * $i = ${i*i} \n"); 
 }
 
@@ -48,11 +50,98 @@ do{
   print(n);
   n++;
 }
-while(n<5);
+while(n<2);
 
 
-while(n<5){
+while(n<2){
   print(n);
   n++;
 }
+
+someFunction();
+someFunction();
+
+sum(1, 2);
+
+
+printPerson(name: "Gogi");
+
+int result = summ2(2, 5);
+print(result);
+int sumResult = result * 2;
+print(sumResult);
+
+
+checkAge(0);
+
+
+operation(2, 3, summOperation);
+operation(2, 3, substractOperation);
+operation(5, 5, multiplyOperation);
+
+
+Function messege = getMessege(11);
+messege();
+messege = getMessege(3);
+messege();
+
+
+Function fn = other();
+fn();
+fn();
+}
+
+
+
+
+void someFunction() => print("monkey");
+
+
+void sum(int a , int b){
+  int c = a + b ;
+  print(c);
+}
+void printPerson({required String name, int? age}){
+  print("Name: $name");
+  print("Age: $age");
+}
+
+int summ2(int f , int g) =>f + g;
+
+void checkAge(int age){
+  if(age < 1 || age > 110)
+  {
+   print("Недействительный возраст");
+   return;
+  }
+  print("Ваш возраст: $age");
+}
+
+
+void operation(int r, int t, Function func){
+  int resultOperation = func(r,t);
+  print(resultOperation);
+}
+
+int summOperation(int r, int t) => r + t;
+int substractOperation(int r, int t) => r - t; 
+int multiplyOperation(int r, int t) => r * t; 
+
+
+getMessege(int hour){
+  if(hour < 12) return morning;
+  else return evening;
+}
+
+void morning() => print("Утречко");
+void evening() => print("Ночи");
+
+
+Function other(){
+  var n = 4;
+  void inner(){
+    n++;
+    print(n);
+  }
+  return inner;
 }
